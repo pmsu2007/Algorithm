263de0c3acacdf03ef5b437e582f0389c80c3c73
unknown <pmsu2007@naver.com>
2021-09-25 01:55:23 +0900

    1439.cpp

---
+++ C++/Greedy_Algorithm/1439.cpp
@@ -0,0 +1,63 @@
+/*
+백준
+뒤집기 1439번
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string bit;
+	cin >> bit;
+
+	int countZero = 0;
+	int countOne = 0;
+
+	char curBit;
+
+	// 묶음 나누기
+	int index = 0;
+	int strlen = bit.length();
+	curBit = bit[index];
+
+	while (index != strlen)
+	{
+		bool change = false;
+		
+
+		if (curBit != bit[index++])
+			change = true;
+		
+		if (change)
+		{
+			if (curBit == '0')
+			{
+				countZero++;
+				curBit = '1';
+			}
+			else
+			{
+				countOne++;
+				curBit = '0';
+			}
+		}
+
+	}
+	if (curBit == bit[strlen - 1])
+	{
+		if (curBit == '1')
+			countOne++;
+		else
+			countZero++;
+	}
+	int ans = min(countOne, countZero);
+	cout << ans;
+}
+
+/*
+010101
+0101011
+*/
